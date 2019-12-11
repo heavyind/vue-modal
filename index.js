717afdd6ce9e18fbb1639ad27552dd27c2a6d353
Chase Ries <hello@chaseries.com>
2019-12-11 03:11:23 -0500

    Get vuex component functionality online
    Puts modal/component show-hide in a working state. API remains lacking.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import modalStore from "./lib/store";
-import VueModal from "./lib/modal";
+import createVueModal from "./lib/modal";
 import e from "./util/e";
 
 
 const cfgDefault = {
   store: null,
+  baseDir: "@/component/modal/",
   scopeName: "modal",
   storeName: "modal",
   componentName: "modal"
@@ -23,13 +24,14 @@ export default {
 
     cfg.store.registerModule(cfg.storeName, modalStore);
 
-    Vue.component(cfg.componentName, VueModal);
+    Vue.component(cfg.componentName, createVueModal(Vue, cfg));
     Vue.mixin({
       computed: {
         [cfg.scopeName] () {
           return {
             open: this.$store.state.modal.open,
             show: () => { this.$store.dispatch(`${cfg.storeName}/show`); },
+            showWith: (c) => { this.$store.dispatch(`${cfg.storeName}/showWith`, c); },
             toggle: () => { this.$store.dispatch(`${cfg.storeName}/toggle`); }
           };
         }
