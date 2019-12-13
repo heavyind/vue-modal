9332e5f87ab45238a33f9c399da6d9cc6944e9b2
Chase Ries <hello@chaseries.com>
2019-12-13 05:49:31 -0500

    Add a little more meat

--- example/ModalTemplate.vue
+++ example/ModalTemplate.vue
@@ -1,11 +1,30 @@
 <template>
-  <div v-if="true"></div>
+  <div v-if="$modal.isOpen" class="modal">
+    <button @click="$modal.close">Close</button>
+    <component :is="$modal.component"></component>
+  </div>
 </template>
 
 <script>
+import { createModalMixin } from "@heavyind/vue-modal";
 
 
+export default {
+  name: "Modal",
+  mixins: [createModalMixin()]
+};
 </script>
 
-<style>
+<style lang="sass">
+
+
+.modal
+  position: fixed
+  color: white
+  top: 0
+  left: 0
+  width: 100vw
+  height: 100vh
+  background: rgba(black, 0.9)
+
 </style>
