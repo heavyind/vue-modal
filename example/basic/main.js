f7470b18b897e115a6e76ecfcbf57d99e1469d42
Chase Ries <hello@chaseries.com>
2019-12-15 03:44:43 -0500

    Add component passing with props to example

--- example/basic/main.js
+++ example/basic/main.js
@@ -1,17 +1,28 @@
 var store = new Vuex.Store({});
+Vue.use(VueModal, { store, mixin: true });
 
 var Modal = {
   template: `
     <div class='modal' v-if='$modal.isOpen'>
       <button @click='$modal.close'>Close</button>
-      <component v-if="$modal.component" :is='$modal.component'></component>
-    </div>`
+      <component
+        v-if="$modal.component"
+        :is="$modal.component"
+        v-bind="$modal.props">
+      </component>
+    </div>`,
+  mounted () {
+    console.log(this.$modal.component);
+  }
 };
 
-Vue.use(VueModal, { store, mixin: true });
+var Dummy = {
+  props: ["greeting"],
+  template: "<div>{{ greeting }}! I am an example modal component.</div>"
+};
 
 new Vue({
   el: "#app",
   store,
-  components: { Modal }
+  components: { Modal, Dummy }
 });
